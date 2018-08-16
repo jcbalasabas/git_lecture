9837e017a9da9da00115fa8e31c41a7f597aaaae
JC Balasabas <JC Balasabas@172.26.83.193>
2018-08-16 16:20:53 +0800

    conflict resolution example

--- GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
@@ -10,6 +10,8 @@ public class NewGitMain {
 		System.out.println("new git class no more");
 		System.out.println("conflict it will not.");
 
+		System.out.println("trying this conflicting thing again... :(");
+
 	}
 
 }
