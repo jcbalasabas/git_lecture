20981a1b3bc400aef72e8c3d9f9c15a3fa46a907
JC Balasabas <JC Balasabas@172.26.83.193>
2018-08-16 16:00:32 +0800

    trying out conflicts

--- GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
@@ -5,6 +5,7 @@ public class NewGitMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new git class");
+		System.out.println("will this conflict?");
 	}
 
 }
