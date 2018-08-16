72339be623ded42b154721b0f6d1eaa007ccbb80
JC Balasabas <JC Balasabas@172.26.83.193>
2018-08-16 16:14:52 +0800

    trying again

--- GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/main/NewGitMain.java
@@ -3,11 +3,10 @@ package com.pointwest.bootcamp.git.main;
 public class NewGitMain {
 
 	public static void main(String[] args) {
-		System.out.println("new git class no more");
-		System.out.println("conflict it will not.");
 		System.out.println("new git class");
 		System.out.println("will this conflict???");
-		
+		System.out.println("new git class no more");
+		System.out.println("conflict it will not.");
 
 	}
 
