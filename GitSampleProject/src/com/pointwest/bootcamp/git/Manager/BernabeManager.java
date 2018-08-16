baf4cb7861b0a3417e2f1ae3869af002d107b30d
User <User@172.16.189.122>
2018-08-16 15:44:13 +0800

    Napush na ulit.

--- GitSampleProject/src/com/pointwest/bootcamp/git/Manager/BernabeManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/Manager/BernabeManager.java
@@ -3,5 +3,6 @@ package com.pointwest.bootcamp.git.Manager;
 public class BernabeManager {
 	public static void main(String[] args) {
 		System.out.println("Edited by Ken.");
+		System.out.println("Sige. Push natin ulit.");
 	}
 }
