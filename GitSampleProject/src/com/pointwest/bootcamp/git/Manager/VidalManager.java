5259b7d8022b6f73152fffc3d52121339a0778e0
User <User@86XBPC2>
2018-08-16 15:38:32 +0800

    Edited by Earl

--- GitSampleProject/src/com/pointwest/bootcamp/git/Manager/VidalManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/Manager/VidalManager.java
@@ -2,7 +2,7 @@ package com.pointwest.bootcamp.git.Manager;
 
 public class VidalManager {
 	public static void main(String[] args) {
-		System.out.println("Hi! This is Kenneth!");
-		System.out.println("This is a newer version.");
+		System.out.println("Hi! This is Kenneth Vidal!");
+		System.out.println("This is edited by Earl.");
 	}
 }
