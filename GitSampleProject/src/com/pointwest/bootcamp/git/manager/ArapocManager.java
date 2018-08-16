dd40774e8feed0e042f697b151124c1133db104d
User <User@172.16.189.129>
2018-08-16 15:52:40 +0800

    committed revisions on syso

--- GitSampleProject/src/com/pointwest/bootcamp/git/manager/ArapocManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/manager/ArapocManager.java
@@ -3,6 +3,8 @@ package com.pointwest.bootcamp.git.manager;
 public class ArapocManager {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Ohayo");
 	}
 
 }
