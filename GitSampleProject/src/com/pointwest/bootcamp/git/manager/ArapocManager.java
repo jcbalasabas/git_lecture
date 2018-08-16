b1c98d5708524ec33d8a610ece40241d65a17e48
User <User@172.16.189.119>
2018-08-16 15:48:34 +0800

    Edited

--- GitSampleProject/src/com/pointwest/bootcamp/git/manager/ArapocManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/manager/ArapocManager.java
@@ -1,6 +1,6 @@
 package com.pointwest.bootcamp.git.manager;
 
-public class ArapocManagerForDelete {
+public class ArapocManager {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
