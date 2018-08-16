2478e1c75b666ee92bf5b0155a0628bc16aa72a2
User <User@172.16.189.122>
2018-08-16 15:02:11 +0800

    This is the most recent version.

--- GitSampleProject/src/com/pointwest/bootcamp/git/Manager/VidalManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/Manager/VidalManager.java
@@ -3,5 +3,6 @@ package com.pointwest.bootcamp.git.Manager;
 public class VidalManager {
 	public static void main(String[] args) {
 		System.out.println("Hi! This is Kenneth!");
+		System.out.println("This is a newer version.");
 	}
 }
