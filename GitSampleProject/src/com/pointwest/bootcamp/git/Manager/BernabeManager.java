598b34b24648effad88f50f4c534ef6fd14702e0
User <User@172.16.189.122>
2018-08-16 15:43:15 +0800

    Edited again

--- GitSampleProject/src/com/pointwest/bootcamp/git/Manager/BernabeManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/Manager/BernabeManager.java
@@ -2,6 +2,6 @@ package com.pointwest.bootcamp.git.Manager;
 
 public class BernabeManager {
 	public static void main(String[] args) {
-		System.out.println("Just edited.");
+		System.out.println("Edited by Ken.");
 	}
 }
