2010514d534e9491c61e5c56a7297089ecb5b6c7
User <User@86XBPC2>
2018-08-16 15:41:25 +0800

    edited by earl

--- GitSampleProject/src/com/pointwest/bootcamp/git/Manager/AguilarManager.java
+++ GitSampleProject/src/com/pointwest/bootcamp/git/Manager/AguilarManager.java
@@ -1,5 +1,8 @@
 package com.pointwest.bootcamp.git.Manager;
 
 public class AguilarManager {
-
+	public static void main(String[] args) {
+		System.out.println("Hi! This is Rey!");
+		System.out.println("This is edited by Earl.");
+	}
 }
